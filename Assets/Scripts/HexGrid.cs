58aaddb0343c142ad4c09b0485b7020a6f8ba4e0
Niels <Niels97vet@hotmail.com>
2016-09-14 12:34:01 +0200

    hex op

---
+++ Assets/Scripts/HexGrid.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class HexGrid : MonoBehaviour {
+
+    public GameObject hexPref;
+
+    public Camera camera;
+
+    private List<GameObject> hexPrefList;
+
+    [SerializeField]
+    private float x;
+    [SerializeField]
+    private float y;
+    [SerializeField]
+    private int size;
+
+    void Start() {
+        makeGrid();
+    }
+
+    void makeGrid() {
+        hexPrefList = new List<GameObject> { };
+
+        for (int i = 0; i < size * size; i++)
+        {
+            hexPrefList.Add(hexPref);
+            hexPrefList[i] = Instantiate(hexPrefList[i], new Vector3((x + y % 2 / 2) * 1.81f, 0, y * 0.866f * 1.81f), Quaternion.identity) as GameObject;
+
+            x++;
+
+            if (x > size - 1)
+            {
+                x = 0;
+                y++;
+            }
+        }
+
+        camera.transform.position = new Vector3(hexPrefList[size / 2].transform.position.x, camera.transform.position.y, hexPrefList[size * size / 2].transform.position.z);
+    }
+
+}
---
+++ Assets/Scripts/MeshSpawner.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MeshSpawner : MonoBehaviour
+{
+    public List<Vector3> Vertices;
+    public Vector2[] UV;
+    public List<int> Triangles;
+
+    private float rad = 0.0174532925f;
+
+    [SerializeField]
+    private Material material;
+
+    [SerializeField]
+    private int sides = 6;
+
+    void Start()
+    {
+        meshSetup();
+        meshDraw();
+    }
+
+    void meshSetup() {
+        Vertices = new List<Vector3> { };
+
+        for(int i = 0; i < sides ; i++)
+        {
+            Vertices.Add(new Vector3(Mathf.Cos((30 + (360 / sides) * i) * rad) * 1f, 0f, Mathf.Sin((30 + (360 / sides) * i) * rad) * 1f));
+        }
+
+        Vertices.Add(Vector3.zero);
+
+        Triangles = new List<int> { };
+
+        for (int i = 0; i < sides; i++)
+        {
+            Triangles.Add(sides);
+                if(i < sides-1)
+                    Triangles.Add(i +1);
+                else
+                    Triangles.Add(0);
+                Triangles.Add(i);
+        }
+    }
+
+    void meshDraw() {
+        MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();
+        gameObject.AddComponent<MeshRenderer>();
+        Mesh mesh = new Mesh();
+
+        mesh.vertices = Vertices.ToArray();
+        mesh.triangles = Triangles.ToArray();
+        mesh.RecalculateNormals();
+
+        meshFilter.mesh = mesh;
+
+        GetComponent<MeshRenderer>().material = material;
+    
+    }
+
+}
